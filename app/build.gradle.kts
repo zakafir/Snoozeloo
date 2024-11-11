193302368fbc3b3d8032cb93666ab86f902cfb05
Zak <zak.afir@gmail.com>
2024-11-11 09:03:11 +0100

    add new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.bundles.koin)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.timber)
 }
