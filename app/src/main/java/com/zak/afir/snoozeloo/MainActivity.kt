6c0c5caa120d31f882b786dea6fd0c1408705a57
Zak <zak.afir@gmail.com>
2024-11-11 09:03:59 +0100

    use NavigationRoot

--- app/src/main/java/com/zak/afir/snoozeloo/MainActivity.kt
+++ app/src/main/java/com/zak/afir/snoozeloo/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.zak.afir.snoozeloo.navigation.NavigationRoot
 import com.zak.afir.snoozeloo.ui.theme.SnoozelooTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +23,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SnoozelooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavigationRoot(
+                        navController = navController
                     )
+
                 }
             }
         }
